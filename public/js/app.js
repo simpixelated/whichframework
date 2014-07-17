8beb198c221342eaa76303f9d0d305b2260fdecc
Jordan Kohl <kohl.jordan@gmail.com>
2014-07-16 20:26:03 -0700

    output to view (html)
    instead of console, actually output the name and homepage using
    Backbone.View

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,38 @@
 define(function (require) {
 	var BB = require('backbone'),
-		_ = require('lodash');
+		_ = require('lodash'),
+		$ = require('jquery');
 
 	var Frameworks = BB.Collection.extend({
 		url: 'learn.json',
 		parse: function (data) {
-			return _.toArray(data);	
+			return _.toArray(data);
+		}
+	});
+
+	var FrameworkView = BB.View.extend({
+		template: function (data) {
+			return '<a href="//'+data.homepage+'">'+data.name+'</a>';
+		},
+		render: function () {
+			this.$el.html(this.template(this.model.attributes));
+			return this;
 		}
 	});
 
     return {
     	start: function () {
     		var frameworks = new Frameworks(),
-    			random;
+    			view;
 
     		frameworks.fetch().done(function (response) {
-    			var random = _.random(0, frameworks.length);
-    			console.log(frameworks.at(random).get('name'));
+    			var random = _.random(0, frameworks.length),
+    				framework = frameworks.at(random);
+
+    			view = new FrameworkView({ model: framework });
+    			$('#main').html(view.render().el);
     		});
+    		
     	}
     };
 });
